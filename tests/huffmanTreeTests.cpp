d8d25faa58db2e72b17eab74173dfc21901c1a3b
jeffinjacob <jeffinjac@gmail.com>
2025-06-14 01:10:38 -0400

    tree tests done

--- tests/huffmanTreeTests.cpp
+++ tests/huffmanTreeTests.cpp
@@ -7,9 +7,9 @@ using namespace std;
 void testOne() {
     unordered_map<unsigned char, uint64_t> map = {
         {'a', 1}, {'b', 2}, {'c', 4}, {'d', 8}};
-    HuffmanTree actualTree = HuffmanTree{map};
-    assert(actualTree.depth() == 3);
-    HuffmanTreeNode *curr = actualTree.getRoot();
+    HuffmanTree huffmanTree = HuffmanTree{map};
+    assert(huffmanTree.depth() == 3);
+    HuffmanTreeNode *curr = huffmanTree.getRoot();
     assert(!curr->ascii && curr->count == 15 && curr->left && curr->right);
     assert(curr->left->ascii &&
            *(curr->left->ascii) == static_cast<unsigned char>('d') &&
@@ -29,7 +29,7 @@ void testOne() {
            curr->count == 1 && !curr->left && !curr->right);
     uint8_t actualLengths[arrSize];
     unordered_map<unsigned char, uint64_t> actualCodes =
-        actualTree.codeTable(actualLengths);
+        huffmanTree.codeTable(actualLengths);
     uint8_t expectedLengths[arrSize] = {};
     expectedLengths[static_cast<unsigned char>('a')] = 3;
     expectedLengths[static_cast<unsigned char>('b')] = 3;
@@ -43,8 +43,68 @@ void testOne() {
     assert(actualCodes == expectedCodes);
 }
 
+void testTwo() {
+    unordered_map<unsigned char, uint64_t> map = {{'a', 1}, {'b', 1}};
+    HuffmanTree huffmanTree = HuffmanTree{map};
+    HuffmanTreeNode *root = huffmanTree.getRoot();
+    assert(!root->ascii && root->count == 2 && root->left && root->right);
+    HuffmanTreeNode *left = root->left;
+    assert(left->ascii && left->count == 1 && !left->left && !left->right);
+    assert(*(left->ascii) == static_cast<unsigned char>('a') ||
+           *(left->ascii) == static_cast<unsigned char>('b'));
+    HuffmanTreeNode *right = root->right;
+    assert(right->ascii && right->count == 1 && !right->left && !right->right);
+    assert(*(right->ascii) == static_cast<unsigned char>('a') ||
+           *(right->ascii) == static_cast<unsigned char>('b'));
+    uint8_t lengths[arrSize];
+    unordered_map<unsigned char, uint64_t> codes =
+        huffmanTree.codeTable(lengths);
+    for (size_t i = 0; i < arrSize; i++) {
+        if (i == static_cast<unsigned char>('a')) {
+            assert(lengths[i] == 1);
+        } else if (i == static_cast<unsigned char>('b')) {
+            assert(lengths[i] == 1);
+        } else {
+            assert(lengths[i] == 0);
+        }
+    }
+    unordered_map<unsigned char, uint64_t> caseOne = {{'a', 0}, {'b', 1}};
+    unordered_map<unsigned char, uint64_t> caseTwo = {{'a', 1}, {'b', 0}};
+    assert(codes == caseOne || codes == caseTwo);
+}
+
+void testThree() {
+    unordered_map<unsigned char, uint64_t> map = {{'\0', 1}};
+    HuffmanTree huffmanTree = HuffmanTree{map};
+    HuffmanTreeNode *root = huffmanTree.getRoot();
+    assert(!root->ascii && root->count == 1 && root->left && root->right);
+    HuffmanTreeNode *left = root->left;
+    assert(left->ascii && *(left->ascii) == static_cast<unsigned char>('\0') &&
+           left->count == 1 && !left->left && !left->right);
+    HuffmanTreeNode *right = root->right;
+    assert(!right->ascii && right->count == 0 && !right->left && !right->right);
+    uint8_t lengths[arrSize];
+    unordered_map<unsigned char, uint64_t> actualCodes =
+        huffmanTree.codeTable(lengths);
+    for (size_t i = 0; i < arrSize; i++) {
+        if (i == static_cast<unsigned char>('\0')) {
+            assert(lengths[i] == 1);
+        } else {
+            assert(lengths[i] == 0);
+        }
+    }
+    unordered_map<unsigned char, uint64_t> expectedCodes = {{'\0', 0}};
+    assert(actualCodes == expectedCodes);
+}
+
 int main() {
+    cout << "\nhuffmanTreeTests: Begin..." << endl;
     testOne();
-    cout << "huffmanTreeTests: PASSED" << endl;
+    cout << "huffmanTreeTests: Passed Test 1" << endl;
+    testTwo();
+    cout << "huffmanTreeTests: Passed Test 2" << endl;
+    testThree();
+    cout << "huffmanTreeTests: Passed Test 3" << endl;
+    cout << "huffmanTreeTests: ...Done" << endl;
     return 0;
 }
