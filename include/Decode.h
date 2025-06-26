17b6a017822348c1dd502bb852d2c36382d743e5
jeffinjacob <jeffinjac@gmail.com>
2025-06-26 19:50:22 -0400

    init decode

---
+++ include/Decode.h
@@ -0,0 +1,14 @@
+#ifndef DECODE_H
+#define DECODE_H
+
+#include "Encode.h"
+
+class Decode {
+    private:
+        std::string src;
+
+    public:
+        Decode(std::string src);
+};
+
+#endif
---
+++ src/Decode.cpp
@@ -0,0 +1,5 @@
+#include "../include/Decode.h"
+
+using namespace std;
+
+Decode::Decode(string src) : src(src) {}
