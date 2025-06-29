addee4927b19d53da0e2ef2ab57efecec1629655
jeffinjacob <jeffinjac@gmail.com>
2025-06-29 01:16:42 -0400

    added macros

--- include/config.h
+++ include/config.h
@@ -1,3 +1,6 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
 #include <cstddef>
 
 inline constexpr size_t CHAR_COUNT = 256, BYTE_SIZE = 8;
@@ -6,3 +9,5 @@ inline constexpr const char *FILE_EXTENSION = ".fin",
                             *FAIL_WRITE_FILE = "failed to write file",
                             *FAIL_INIT_TREE = "failed to initialize "
                                               "huffman tree";
+
+#endif
